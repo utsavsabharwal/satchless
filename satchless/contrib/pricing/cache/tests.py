fa6698d8f8aae6b08790607ca0ae0def3f7008fd
Michal Ociepka <michal@ociepka.info>
2012-06-26 11:52:55 +0200

    Fix #18, tests for pricing cache

--- satchless/contrib/pricing/cache/tests.py
+++ satchless/contrib/pricing/cache/tests.py
@@ -6,6 +6,27 @@ from ....contrib.pricing.cache import PricingCacheHandler
 
 from decimal import Decimal
 
+class CacheTestHandler(ProductFieldGetter):
+    def get_variant_price(self, variant, currency, quantity=1, **kwargs):
+        price = super(CacheTestHandler, self).get_variant_price(variant,
+                                                currency, quantity, **kwargs)        
+        if not hasattr(price, '_cache'):
+            price._cache = 0
+        else:
+            price._cache += 1
+            
+        return price
+
+    def get_product_price_range(self, product, currency, **kwargs):
+        price = super(CacheTestHandler, self).get_product_price_range(product,
+                                                            currency, **kwargs)        
+        if not hasattr(price, '_cache'):
+            price._cache = 0
+        else:
+            price._cache += 1
+        
+        return price
+
 class Cache(TestCase):
     def setUp(self):
         self.parrot = DeadParrot.objects.create(slug='parrot', species='Parrot')
@@ -19,10 +40,11 @@ class Cache(TestCase):
                                                         looks_alive=True)
         self.cockatoo.price = Decimal(20)
         self.cockatoo_a.price = Decimal(21)
-        self.cached_queue = PricingQueue(PricingCacheHandler(ProductFieldGetter))
+        self.pricing_cache_handler = PricingCacheHandler(CacheTestHandler)
+        self.cached_queue = PricingQueue(self.pricing_cache_handler)
         self.non_cached_queue = PricingQueue(ProductFieldGetter)
 
-    def test_variant_price(self):
+    def test_variant_price_add_to_cache(self):
         p0 = self.non_cached_queue.get_variant_price(self.parrot_a, None)
         p1 = self.cached_queue.get_variant_price(self.parrot_a, None)
         self.assertEqual(p0, p1)
@@ -31,11 +53,45 @@ class Cache(TestCase):
         p1 = self.cached_queue.get_variant_price(self.cockatoo_a, None)
         self.assertEqual(p0, p1)
 
-    def test_product_price_range(self):
+    def test_product_price_range_add_to_cache(self):
         p0 = self.non_cached_queue.get_product_price_range(self.parrot)
         p1 = self.cached_queue.get_product_price_range(self.parrot)
         self.assertEqual(p0, p1)
-
-        p0 = self.non_cached_queue.get_product_price_range(self.cockatoo)
+        
+        p0 = self.non_cached_queue.get_product_price_range(self.cockatoo)         
         p1 = self.cached_queue.get_product_price_range(self.cockatoo)
         self.assertEqual(p0, p1)
+        
+    def test_variant_price_get_from_cache(self):
+        p0 = self.non_cached_queue.get_variant_price(self.parrot_a, None)
+        p1 = self.cached_queue.get_variant_price(self.parrot_a, None)
+        
+        self.assertEqual(p0, p1)
+        self.assertEqual(p1._cache, 0)
+        
+        p0 = self.non_cached_queue.get_variant_price(self.cockatoo_a, None)
+        p1 = self.cached_queue.get_variant_price(self.cockatoo_a, None)
+        
+        self.assertEqual(p0, p1)
+        self.assertEqual(p1._cache, 0)
+
+    def test_product_price_range_get_from_cache(self):
+        p0 = self.non_cached_queue.get_product_price_range(self.parrot)
+        p1 = self.cached_queue.get_product_price_range(self.parrot)
+        
+        self.assertEqual(p0, p1)
+        self.assertEqual(p1._cache, 0)
+        
+        p0 = self.non_cached_queue.get_product_price_range(self.cockatoo)
+        p1 = self.cached_queue.get_product_price_range(self.cockatoo)
+        
+        self.assertEqual(p0, p1)
+        self.assertEqual(p1._cache, 0)
+                
+    def test_product_cache_key_generator(self):
+        key = 'currency=None&product=%s'%self.parrot.id
+        p = self.cached_queue.get_product_price_range(self.parrot)
+        key_from_p = self.pricing_cache_handler._get_key(currency=None,
+                                                         product=self.parrot)
+        
+        self.assertEqual(key, key_from_p)
