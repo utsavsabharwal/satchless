3579c6a14da8380b29bfab1887a7925257381ef9
Marek Brzóska <brzoskamarek@gmail.com>
2012-06-26 13:34:03 +0200

    add variants

--- examples/tutorial/products/models.py
+++ examples/tutorial/products/models.py
@@ -8,6 +8,7 @@ from satchless.contrib.pricing.simpleqty.models import ProductPriceMixin,\
     VariantPriceOffsetMixin
 from satchless.contrib.tax.flatgroups.models import TaxedProductMixin
 from satchless.util.models import construct
+import satchless
 import satchless.product.models
 
 from ..categories.models import Category
@@ -20,16 +21,91 @@ class Product(ProductPriceMixin, TaxedProductMixin,
     slug = models.SlugField(_('slug'), max_length=128)
     description = models.TextField(_('description'), blank=True)
 
+
+class Variant(satchless.product.models.Variant, VariantPriceOffsetMixin):
+    pass
+
+
+class OnlineVariant(Variant):
+    file = models.FileField()
+    file_format = models.CharField(max_length=4)
+
+    class Meta:
+        abstract = True
+
+
+class OfflineVariant(Variant):
+    stock = models.PositiveIntegerField()
+
+    class Meta:
+        abstract = True
+
+
 class Book(Product):
     isbn = models.CharField(max_length=13)
     author = models.CharField(max_length=50)
     publication_date = models.DateField(blank=True)
 
+
+class BookVariant(Variant):
+    book = models.ForeignKey(Book)
+
+    class Meta:
+        abstract = True
+
+
+class EBookVariant(BookVariant, OnlineVariant):
+    pass
+
+
+class TraditionalBookVariant(BookVariant, OfflineVariant):
+    hard_cover = models.BooleanField(default=False)
+
+
 class Movie(Product):
     director = models.CharField(max_length=50)
     premiere_date = models.DateField(blank=True)
+
+
+class MovieVariant(Variant):
+    movie = models.ForeignKey(Movie)
+
+    class Meta:
+        abstract = True
+
+
+class OnlineMovieVariant(MovieVariant, OnlineVariant):
+    with_adverts = models.BooleanField(default=False)
+
+
+class TraditionalMovie(MovieVariant, OfflineVariant):
+    CARRIERS = (
+        ('vhs', 'VHS'),
+        ('dvd', 'DVD'),
+        ('br', 'BlueRay'),
+    )
+    carrier = models.CharField(choices=CARRIERS)
+
 
 class Music(Product):
     author = models.CharField(max_length=50)
     publisher = models.CharField(max_length=50)
 
+
+class MusicVariant(Variant):
+    music = models.ForeignKey(Music)
+
+    class Meta:
+        abstract = True
+
+
+class MusicFileVariant(MusicVariant, OnlineVariant):
+    duration = models.IntegerField()
+
+
+class MusicAlbumVariant(MusicVariant, OfflineVariant):
+    CARRIERS = (
+        ('dvd', 'DVD'),
+        ('br', 'BlueRay'),
+        ('cd', 'Audio CD'))
+    carrier = models.CharField(choices=CARRIERS)
