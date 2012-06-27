1cc6365f3ca9a832a906ba47499069716529b34c
Michal Ociepka <michal@ociepka.info>
2012-06-27 16:54:26 +0200

    Create app and templates for products list

--- examples/tutorial/products/app.py
+++ examples/tutorial/products/app.py
@@ -1,13 +1,25 @@
 import satchless.category.app
 from satchless.pricing.app import ProductAppPricingMixin
+from satchless.product.app import ProductApp
+from satchless.core.app import view
 
 from . import models
-import products.models
+from products.models import Product, Variant
 
-class CategorizedProductApp(ProductAppPricingMixin,
-                            satchless.category.app.CategorizedProductApp):
-    Category = models.Category
-    Product = products.models.Product
-    Variant = object
+from django.template.response import TemplateResponse
 
-product_app = CategorizedProductApp()
+class ProductsApp(ProductApp):
+    Product = Product
+    Variant = Variant
+    #Category = models.Category
+
+    @view(r'^$', name='products-index')
+    def product_list(self, request, products = None):
+        if not products:
+            products = self.Product.objects.all()
+        context = self.get_context_data(request, products=products)
+        template = 'satchless/products/list.html'
+        return TemplateResponse(request, template, context)
+
+#product_app = CategorizedProductApp()
+products_app = ProductsApp()
