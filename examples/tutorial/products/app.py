8660fe35df8e3c985a6dcfa3927575c42beca04e
Michal Ociepka <michal@ociepka.info>
2012-06-28 13:54:45 +0200

    Improve product app and product templates

--- examples/tutorial/products/app.py
+++ examples/tutorial/products/app.py
@@ -11,15 +11,28 @@ from django.template.response import TemplateResponse
 class ProductsApp(ProductApp):
     Product = Product
     Variant = Variant
-    #Category = models.Category
 
     @view(r'^$', name='products-index')
     def product_list(self, request, products = None):
         if not products:
             products = self.Product.objects.all()
         context = self.get_context_data(request, products=products)
-        template = 'satchless/products/list.html'
+        template = 'product/list.html'
+        
         return TemplateResponse(request, template, context)
 
-#product_app = CategorizedProductApp()
+    @view(r'^\+(?P<product_pk>[0-9]+)-(?P<product_slug>[a-z0-9_-]+)/$',
+          name='details')
+    def product_details(self, request, **kwargs):
+        tmpl_resp = super(ProductsApp, self).product_details(request, **kwargs)
+        
+        product = tmpl_resp.context_data['product']
+        variants = [variant.get_subtype_instance() for variant in
+                    self.Variant.objects.filter(product = product)]
+
+        tmpl_resp.context_data['variants'] = variants
+        tmpl_resp.template_name = 'product/%s.html'%product._meta.module_name
+
+        return tmpl_resp
+
 products_app = ProductsApp()
