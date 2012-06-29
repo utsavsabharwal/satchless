32bd40b6bfb06ff06713b4eaaff46d8ba919e5ae
Michal Ociepka <michal@ociepka.info>
2012-06-29 16:06:58 +0200

    Add auto generate slug from name

--- examples/tutorial/categories/admin.py
+++ examples/tutorial/categories/admin.py
@@ -2,4 +2,7 @@
 from django.contrib import admin
 from categories.models import Category
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
