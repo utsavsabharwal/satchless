24ef206c682f77de0a4da949ce420945608d1610
Marek Brzóska <brzoskamarek@gmail.com>
2012-06-29 16:45:07 +0200

    Add cart app and models

---
+++ examples/tutorial/carts/app.py
@@ -0,0 +1,31 @@
+from . import models
+from django.shortcuts import redirect
+from django.utils.decorators import method_decorator
+from django.views.decorators.http import require_POST
+from examples.tutorial.products.models import Variant
+from satchless.cart.app import MagicCartApp
+from satchless.core.app import view
+
+
+class CartApp(MagicCartApp):
+    Cart = models.Cart
+    CartItem = models.CartItem
+
+    @view(r'^add/$', name='add-item')
+    @method_decorator(require_POST)
+    def add_item_view(self, request):
+        cart = self.get_cart_for_request(request)
+        post = request.POST
+        variant_id = int(post.get('variant_id'))
+        try:
+            quantity = int(post.get('quantity'))
+        except ValueError:
+            quantity = 1
+        redirect_url = (
+            post.get('redirect_url')
+            or request.META.get('HTTP_REFERER')
+            or '')  # TODO it would be smarter to get product url or cart url
+        variant = Variant.objects.get(variant_id)
+        variant = variant.get_subtype_instance()
+        cart.add_item(variant, quantity)
+        return redirect(redirect_url)
--- examples/tutorial/carts/models.py
+++ examples/tutorial/carts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from examples.tutorial.products.models import Variant
+from satchless.cart.models import Cart, CartItem
+from satchless.util.models import construct
+
+
+class Cart(Cart):
+    pass
+
+
+class CartItem(construct(CartItem, cart=Cart, variant=Variant)):
+    pass
