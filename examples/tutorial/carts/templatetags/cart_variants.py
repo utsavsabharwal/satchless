b1c497405c531c4ad8ff2e427c964c7325b7ebfa
Michal Ociepka <michal@ociepka.info>
2012-07-02 16:44:11 +0200

    Add cart templatetag

--- examples/tutorial/carts/templatetags/cart_variants.py
+++ examples/tutorial/carts/templatetags/cart_variants.py
@@ -3,8 +3,8 @@ from django.template import Library
 from ..app import cart_app
 register = Library()
 
-@register.inclusion_tag('cart/_variants_forms.html', takes_context = True)
-def variants_to_cart_forms(context, variants, product):
+@register.inclusion_tag('cart/_variants_forms.html')
+def variants_to_cart_forms(variants, product):
     forms = {}
     for variant in variants:
         forms[variant.__class__]= variant.Form(product = product,
@@ -18,3 +18,14 @@ def variants_to_list(variants, product):
 @register.simple_tag
 def cart_form_target():
     return cart_app.reverse('add-item')
+
+@register.simple_tag
+def cart_target():
+    return cart_app.reverse('details')
+
+@register.inclusion_tag('cart/_view.html', takes_context = True)
+def cart(context):
+    if 'request' not in context:
+        return
+    cart = cart_app.get_cart_for_request(context['request'])
+    return {'items': cart.get_all_items()}
--- examples/tutorial/settings.py
+++ examples/tutorial/settings.py
@@ -46,32 +46,32 @@ USE_I18N = False
 USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
+# Example: '/home/media/media.lawrence.com/media/'
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+# Examples: 'http://media.lawrence.com/media/', 'http://example.com/media/'
 MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
+# in apps' 'static/' subdirectories and in STATICFILES_DIRS.
+# Example: '/home/media/media.lawrence.com/static/'
 STATIC_ROOT = ''
 
 # URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
+# Example: 'http://media.lawrence.com/static/'
 STATIC_URL = '/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
+# Examples: 'http://foo.com/static/admin/', '/static/admin/'.
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, "static"),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -103,11 +103,11 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'tutorial.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Put strings here, like '/home/html/django_templates' or 'C:/www/django/templates'.
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     #os.path.join(PROJECT_ROOT, 'templates'),
-    os.path.join(PROJECT_ROOT, "templates"),
+    os.path.join(PROJECT_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -117,15 +117,16 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    
+    #'satchless.pricing',
+    #'satchless.product',
+    'satchless.contrib.tax.flatgroups',
+    #'satchless.cart',
+
     'products',
     'categories',
     'carts',
-    'satchless.contrib.tax.flatgroups',
-    
 )
 
 # A sample logging configuration. The only tangible logging
@@ -150,6 +151,18 @@ LOGGING = {
         },
     }
 }
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request'
+)
+
+INTERNAL_IPS = ['127.0.0.1']
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
