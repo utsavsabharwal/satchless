42bf4285924ad2968bb2ef0f2f6d080900e4f043
Marek Brzóska <brzoskamarek@gmail.com>
2012-07-02 14:02:21 +0200

    Add total price to cart.

--- examples/tutorial/carts/models.py
+++ examples/tutorial/carts/models.py
@@ -1,15 +1,20 @@
 from products.models import Variant
 from products import pricing
 from satchless.cart.models import Cart, CartItem
+from satchless.pricing import Price
 from satchless.util.models import construct
 
 
 class Cart(Cart):
-    pass
+    def total(self, handler=pricing.pricing_handler):
+        print 'aaaaaaaaaaaaaaaaaa'
+        print ([item.price(handler) for item in self.get_all_items()])
+        return sum(
+            [item.price(handler) for item in self.get_all_items()],
+            Price(0)
+        )
 
 
 class CartItem(construct(CartItem, cart=Cart, variant=Variant)):
-    @property
     def price(self, handler=pricing.pricing_handler):
         return handler.get_variant_price(self.variant, quantity=self.quantity)
-
